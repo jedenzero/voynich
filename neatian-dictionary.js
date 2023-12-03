29d63eda5b1f60d7c77c74515344f28bc294ed0d
jedenzero <134185805+jedenzero@users.noreply.github.com>
2023-12-03 20:33:13 +0900

    Update neatian-dictionary.js

--- neatian-dictionary.js
+++ neatian-dictionary.js
@@ -1,8 +1,15 @@
 var dictionary = [
-        {word:"veăšfil",meaning:"달빛",origin:"vex+šfil",comment:"",ex:"Veăšfilem šfile.<br>달빛이 반짝인다."},
-        {word:"vex",meaning:"달",ex:"Vexeč once.<br>달을 본다."},
-        {word:"ijot",meaning:"하늘",ex:"Ijotem ičlaje.<br>하늘이 참 푸르다."},
-        {word:"lazen",meaning:"꽃",ex:"Sysontea lazen.<br>분분한 낙화."},
-        {word:"neă",meaning:"산, 산맥",ex:"Neăjan ečonce, zočej<br>저 산맥은, 말도 없이"},
-        {word:"šljo",meaning:"바람",ex:"Šljojan znjeke.<br>바람이 그치지 않는다."}
-      ];
+  {word:"veăšfil",meaning:"달빛",origin:"vex+šfil",ex:"Veăšfilem šfile.<br>달빛이 반짝인다."},
+  {word:"vex",meaning:"달",ex:"Vexeč once.<br>달을 본다."},
+  {word:"ijot",meaning:"하늘",ex:"Ijotem ičlaje.<br>하늘이 참 푸르다."},
+  {word:"lazen",meaning:"꽃",ex:"Sysontea lazen.<br>분분한 낙화."},
+  {word:"neă",meaning:"산, 산맥",ex:"Neăjan ečonce, zočej<br>저 산맥은, 말도 없이"},
+  {word:"šljo",meaning:"바람",ex:"Šljojan znjeke.<br>바람이 그치지 않는다."},
+  {word:"žvje",meaning:"나무",ex:"Žvjejan ɣimvežal ijez,<br>나무는 고요하고자 하나,"},
+  {word:"neăx",meaning:"산안개",ex:"Neăša neăx ake.<br>산 주위를 안개가 두른다."},
+  {word:"lomve",meaning:"웃다, 행복하다",ex:"Lomve je, ize asve ze lomvesul.<br>웃어라, 온 세상이 너와 함께 웃을 것이다."},
+  {word:"ymve",meaning:"슬프다, 울다",ex:"Ymve je, ize es ejymvesul.<br>울어라, 너 혼자가 울 것이다."},
+  {word:"ake",meaning:"있다",ex:"Šfilan akeči.<br>빛이 있으라."},
+  {word:"člaje",meaning:"넓다, 푸르다",ex:"Vjej člajea isni<br>창백한 푸른 점"},
+  {word:"ačen",meaning:"화살",ex:"Ačeneze!<br>화살이다!"}
+];
