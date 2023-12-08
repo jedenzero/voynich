d2a1928743635a0fdcdb0e53bb4cbcac86ba574b
jedenzero <134185805+jedenzero@users.noreply.github.com>
2023-12-08 21:52:33 +0900

    Update neatian-writings.js

--- neatian-writings.js
+++ neatian-writings.js
@@ -1,3 +1,3 @@
 var list=[
-    {id:"단문 1",text:"Šfil sonte.<br>별이 떨어진다."}
+    {id:"단문 1",text:'<img style="width:200px;" src="https://unsplash.com/ko/%EC%82%AC%EC%A7%84/%ED%91%B8%EB%A5%B8-%ED%95%98%EB%8A%98-%EC%95%84%EB%9E%98-%EB%B3%84%EB%98%A5%EB%B3%84-TIrXot28Znc"><p style="text-align:center;">Šfil sonte.<br>별이 떨어진다.</p>'}
 ];
