40a6a2de42378ff9ddc016d016e238ff12f730ec
jedenzero <134185805+jedenzero@users.noreply.github.com>
2023-12-09 12:47:52 +0900

    Update neatian-writings.js

--- neatian-writings.js
+++ neatian-writings.js
@@ -1,3 +1,4 @@
 var list=[
-    {id:"단문 1",text:'<img style="width:200px;" src="https://unsplash.com/ko/%EC%82%AC%EC%A7%84/%ED%91%B8%EB%A5%B8-%ED%95%98%EB%8A%98-%EC%95%84%EB%9E%98-%EB%B3%84%EB%98%A5%EB%B3%84-TIrXot28Znc"><p style="text-align:center;">Šfil sonte.<br>별이 떨어진다.</p>'}
+    {id:"단문 1 - 유성",text:'<img style="width:200px;margin:0 auto;" src="https://images.unsplash.com/uploads/14116941824817ba1f28e/78c8dff1?q=80&w=686&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"><p style="text-align:center;">Šfily sonte.<br>별이 떨어진다.</p>'},
+    {id:"단문 2 - 크리스마스",text:'<img style="width:200px;margin:0 auto;" src="https://images.unsplash.com/photo-1544235653-a313b8a430d9?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHdoaXRlJTIwY2hyaXN0bWFzfGVufDB8fDB8fHwy"><p style="text-align:center;">Ličea kistemas<br>화이트 크리스마스</p>'}
 ];
