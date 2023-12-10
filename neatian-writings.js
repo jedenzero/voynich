10060a713fe82a9e54f384ae23453c9fdd3c0f0d
jedenzero <134185805+jedenzero@users.noreply.github.com>
2023-12-10 22:33:28 +0900

    Update neatian-writings.js

--- neatian-writings.js
+++ neatian-writings.js
@@ -1,4 +1,5 @@
 var list=[
     {id:"단문 1 - 유성",text:'<img style="width:200px;margin:0 auto;display:block;" src="https://images.unsplash.com/uploads/14116941824817ba1f28e/78c8dff1?q=80&w=686&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"><p style="text-align:center;">Šfily sonte.<br>별이 떨어진다.</p>'},
-    {id:"단문 2 - 크리스마스",text:'<img style="width:200px;margin:0 auto;display:block;" src="https://images.unsplash.com/photo-1544235653-a313b8a430d9?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHdoaXRlJTIwY2hyaXN0bWFzfGVufDB8fDB8fHwy"><p style="text-align:center;">Ličea kistemas<br>화이트 크리스마스</p>'}
+    {id:"단문 2 - 크리스마스",text:'<img style="width:200px;margin:0 auto;display:block;" src="https://images.unsplash.com/photo-1544235653-a313b8a430d9?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHdoaXRlJTIwY2hyaXN0bWFzfGVufDB8fDB8fHwy"><p style="text-align:center;">Ličea kistemas<br>화이트 크리스마스</p>'},
+    {id:"단문 3 - 달",text:'<img style="width:200px;margin:0 auto;display:block;" src="https://images.unsplash.com/photo-1535536609565-9675cafeff32?q=80&w=688&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"><p style="text-align:center;">Vex ezlone, kse asveč ksule once.<br>저 달은 비록 작으나 온 천하를 비추는구나.</p>'}
 ];
