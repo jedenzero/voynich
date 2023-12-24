08cce122223e01784f7463b96fc0a74ee4f8f318
jedenzero <134185805+jedenzero@users.noreply.github.com>
2023-12-24 16:32:16 +0900

    Update cantonese2-dictionary.js

--- cantonese2-dictionary.js
+++ cantonese2-dictionary.js
@@ -1,3 +1,13 @@
 var dictionary = [
-  {word:"月",meaning:"달",ex:"月係暝嘅太陽。<br>달은 밤의 태양이다."}
+  {word:"月",pronounce:"jyut",meaning:"달",ex:"月係暝嘅太陽。<br>달은 밤의 태양이다."},
+  {word:"淚",pronounce:"leoi",meaning:"눈물",ex:"<br>"},
+  {word:"車",pronounce:"ce",meaning:"자동차",ex:"<br>"},
+  {word:"腳",pronounce:"goek",meaning:"다리(건물)",ex:"<br>"},
+  {word:"闔",pronounce:"haap",meaning:"기둥, 문, 벽",ex:"<br>"},
+  {word:"話",pronounce:"waa",meaning:"대화, 말, 언어",ex:"<br>"},
+  {word:"紙",pronounce:"zi",meaning:"종이",ex:"<br>"},
+  {word:"今",pronounce:"gam",meaning:"금방, 막, 방금, 이제",ex:"<br>"},
+  {word:"互",pronounce:"wu",meaning:"서로",ex:"<br>"},
+  {word:"相",pronounce:"상대, 상대방",meaning:"",ex:"<br>"},
+  {word:"飯",pronounce:"먹이, 밥, 식사",meaning:"",ex:"<br>"}
 ];
