05539c2bc1377747360a9525abe5edd6b36c3657
jedenzero <134185805+jedenzero@users.noreply.github.com>
2023-12-27 22:17:56 +0900

    Update neatian-list.js

--- neatian/neatian-list.js
+++ neatian/neatian-list.js
@@ -4,3 +4,27 @@ var list=[
     {type:"wr", title:"단문 2 - 크리스마스"},
     {type:"wr", title:"고독 - Ella Wheeler Wilcox"}
 ]
+
+function make() {
+    var result="<h3><b>강좌</b></h3>";
+    list.forEach(function(item) {
+        if (item.type==="ls")
+            result+='<p style="padding-left:5px;"><a href="'+item.title+'.html">'+item.title+"</a></p>";
+         });
+    document.getElementById("imsi1").innerHTML=result;
+          
+    result="<h3><b>도구</b></h3>";
+    list.forEach(function(item) {
+        if (item.type==="tl")
+            result+='<p><a href="'+item.title+'.html">'+item.title+"</a></p>";
+    });
+    document.getElementById("imsi2").innerHTML=result;
+          
+    result="<h3><b>글</b></h3>";
+    list.forEach(function(item) {
+        if (item.type==="wr")
+            result+='<p><a href="'+item.title+'.html">'+item.title+"</a></p>";
+    });
+    document.getElementById("imsi3").innerHTML=result;
+    }
+    make();
