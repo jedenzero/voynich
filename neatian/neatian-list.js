fd22b6a675a11087cded5d932d2a1dd278b46e3e
jedenzero <134185805+jedenzero@users.noreply.github.com>
2023-12-30 21:42:54 +0900

    Update neatian-list.js

--- neatian/neatian-list.js
+++ neatian/neatian-list.js
@@ -1,8 +1,9 @@
 var list=[
-    {type:"ls",title:"1. 네어트어란"},
+    {type:"ls",title:"0. 네어트어란"},
     {type:"wr", detail:"작문", title:"단문 1 - 유성"},
     {type:"wr", detail:"작문", title:"단문 2 - 크리스마스"},
-    {type:"wr", detail:"번역", title:"고독 - Ella Wheeler Wilcox"}
+    {type:"wr", detail:"번역", title:"단문 3 - 달"},
+    {type:"wr", detail:"번역", title:"고독 - Ella Wheeler Wilcox"} 
 ]
 
 function make() {
