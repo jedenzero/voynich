619474a1ed8a8f78c0159f37fb73e3d9dc2398cb
jedenzero <134185805+jedenzero@users.noreply.github.com>
2024-01-19 21:42:52 +0900

    Update neatian-list.js

--- neatian/neatian-list.js
+++ neatian/neatian-list.js
@@ -3,7 +3,8 @@ var list=[
     {type:"wr", detail:"작문", title:"단문 1 - 유성"},
     {type:"wr", detail:"작문", title:"단문 2 - 크리스마스"},
     {type:"wr", detail:"번역", title:"단문 3 - 달"},
-    {type:"wr", detail:"번역", title:"고독 - Ella Wheeler Wilcox"} 
+    {type:"wr", detail:"번역", title:"고독 - Ella Wheeler Wilcox"},
+    {type:"wr", detail:"개발 과정", title:"접미사가 붙은 동사"}
 ]
 
 function make() {
